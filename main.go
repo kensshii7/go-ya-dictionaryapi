55810ca614627de1a3244267f993df935ee9180a
Ainur Valeev <prodyxxxy@gmail.com>
2020-09-06 20:22:46 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	dictionaryApi := Init("dict.1.1.20200906T162446Z.618894a4672590dc.b0914b4496701dec37a2a9338404638b49f56026")
+	dictionaryApi := Init("API")
 
 	buffer, _ := dictionaryApi.Lookup("Привет", "ru-en", "", FLAGS_FAMILY)
 	fmt.Println(buffer)
